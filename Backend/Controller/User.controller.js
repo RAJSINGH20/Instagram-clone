c695e48fea02226e4082f3d8ac9a912b4ab92faa
Raj Singh <rajsinghdurgapur26@gmail.com>
2025-12-05 09:57:21 +0530

    add user registration and login functionality

---
+++ Backend/Controller/User.controller.js
@@ -0,0 +1,60 @@
+import { User } from "../Models/User.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const Register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        if (!username || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" })
+        }
+
+        const user = await User.findOne({ email })
+
+        if (user) {
+            return res.status(400).json({ message: "User already exists" })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+
+
+        const newUser = new User({ username, email, password: hashedPassword })
+        await newUser.save();
+
+
+
+
+        res.status(200).json({ message: "User registered successfully", success: true })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Internal server error", success: false })
+    }
+}
+
+
+export const Login = async (req, res) => {
+    try {
+        const{ email, password } = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({ message: "All fields are required" })
+        }
+
+        const user = await User.findOne({ email })
+
+        if(!user){
+            return res.status(400).json({ message: "User does not exist" })
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid){
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" })
+        res.status(200).json({ message: "Login successful", success: true, user, token })
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", success: false })
+    }
+}
